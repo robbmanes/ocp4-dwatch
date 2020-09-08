e6f311e5ffaaafc0b5cdd2e85f52ebf241abb6b9
Robb Manes <rmanes@redhat.com>
2020-09-08 16:50:03 -0400

    fix awk processing of sched_debug

--- rhcos-dwatch.sh
+++ rhcos-dwatch.sh
@@ -40,7 +40,7 @@ function check_sched_debug()
 function parse_sched_debug()
 {
 	echo "$(date) - $HOST"
-	/usr/bin/awk '/^\ D/' $PROCPATH/sched_debug | while read LINE
+	/usr/bin/awk '/^ D/' $PROCPATH/sched_debug | while read LINE
 	do
 		# do this ASAP, the condition may clear!
 		PID=$(echo $LINE | awk '{print $3}')
